3cc9258dbe030fedf85329c25296deafc5e7193a
Prathibha Ratnayake <prsthibha@gmail.com>
2020-09-15 12:12:12 +0530

    Login component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Login from "./components/Login";
 
 function App() {
-  return (
-    <div className="app">
-      
-    </div>
-  );
+  return <div className='app'>
+    {/* spotify logo */}
+    {/* Login with spotify button */}
+    <Login/>
+  </div>;
 }
 
 export default App;
