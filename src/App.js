a68b3ceabebd814301151e52985304fde860f60f
Prathibha Ratnayake <prsthibha@gmail.com>
2020-09-15 19:45:09 +0530

    auth token

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./components/Login";
+import { getTokenFromUlr } from "./spotify";
 
 function App() {
-  return (
-    <div className='app'>
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  // run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUlr();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+    console.log("i have a token >> ", token);
+  }, []);
+
+  return <div className='app'>{token ? <h1>logged in</h1> : <Login />}</div>;
 }
 
 export default App;
