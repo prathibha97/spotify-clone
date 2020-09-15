db37cfeaf49c655c3fd6b50c004d955d3a87cc42
Prathibha Ratnayake <prsthibha@gmail.com>
2020-09-15 19:49:31 +0530

    spotify-web-api added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./components/Login";
 import { getTokenFromUlr } from "./spotify";
+import SpotityWebApi from "spotify-web-api-js";
+
+const spotify = new SpotityWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -13,6 +16,7 @@ function App() {
     const _token = hash.access_token;
     if (_token) {
       setToken(_token);
+      spotify.setAccessToken(_token);
     }
     console.log("i have a token >> ", token);
   }, []);
