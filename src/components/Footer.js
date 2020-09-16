439b237ff3110b472d8403e928364e64fbfc1dc6
Prathibha Ratnayake <prsthibha@gmail.com>
2020-09-16 16:04:55 +0530

    footer volume control added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,9 @@ import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
 import SkipNextIcon from "@material-ui/icons/SkipNext";
 import ShuffleIcon from "@material-ui/icons/Shuffle";
 import RepeatIcon from "@material-ui/icons/Repeat";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import { Grid, Slider } from "@material-ui/core";
 
 function Footer() {
   return (
@@ -22,7 +25,17 @@ function Footer() {
       </div>
 
       <div className='footer__right'>
-        <p>volume control</p>
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
