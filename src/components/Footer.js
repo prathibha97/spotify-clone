cb49ba87266e92f198a46343fa1c782354a62cbc
Prathibha Ratnayake <prsthibha@gmail.com>
2020-09-16 15:54:16 +0530

    music control icons added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import '../css/Footer.css'
+import React from "react";
+import "../css/Footer.css";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
 
 function Footer() {
-    return (
-        <div className='footer'>
-            <h1>footer</h1>
-        </div>
-    )
+  return (
+    <div className='footer'>
+      <div className='footer__left'>
+        <p>album & song details</p>
+      </div>
+
+      <div className='footer__center'>
+        <ShuffleIcon className='footer__green' />
+        <SkipPreviousIcon className='footer__icon' />
+        <PlayCircleOutlineIcon fontSize='large' className='footer__icon' />
+        <SkipNextIcon className='footer__icon' />
+        <RepeatIcon className='footer__green' />
+      </div>
+
+      <div className='footer__right'>
+        <p>volume control</p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
