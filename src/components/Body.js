fd8a7b13e595e9779f63095a42a9499a48adc0eb
Prathibha Ratnayake <prsthibha@gmail.com>
2020-09-16 17:36:50 +0530

    body__icons styled

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,29 @@
+import { Favorite, MoreHoriz, PlayCircleFilled } from "@material-ui/icons";
 import React from "react";
+import { StateProviderValue } from "../context/StateProvider";
 import "../css/Body.css";
 import Header from "./Header";
 
 function Body({ spotify }) {
+  const [{ discover_weekly }, dispatch] = StateProviderValue();
   return (
     <div className='body'>
       <Header spotify={spotify} />
-      <div className="body__info">
-          <img src="" alt=""/>
-          <div className="body__infoText">
-              <strong>PLAYLIST</strong>
-              <h2>Discover Weekly</h2>
-              <p>description...</p>
-          </div>
+      <div className='body__info'>
+        <img src={discover_weekly?.images[0]?.url} alt='' />
+        <div className='body__infoText'>
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>{discover_weekly?.description}</p>
+        </div>
+      </div>
+      <div className='body__songs'>
+        <div className='body__icons'>
+          <PlayCircleFilled className='body__shuffle'/>
+          <Favorite className='large'/>
+          <MoreHoriz/>
+        </div>
+        {/* list of songs */}
       </div>
     </div>
   );
