b8a24c50deccf6ebfeac63c4f7b439641db352f5
Prathibha Ratnayake <prsthibha@gmail.com>
2020-09-16 15:19:44 +0530

    sideBar playlist styled

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,14 +1,28 @@
 import React from "react";
 import "../css/SideBar.css";
 import SideBarOptions from "./SideBarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
 function SideBar() {
   return (
     <div className='sideBar'>
-        <img className='sideBar__logo' src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
-        <SideBarOptions title='Home'/>
-        <SideBarOptions title='Search'/>
-        <SideBarOptions title='Your Library'/>
+      <img
+        className='sideBar__logo'
+        src='https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg'
+        alt=''
+      />
+      <SideBarOptions Icon={HomeIcon} title='Home' />
+      <SideBarOptions Icon={SearchIcon} title='Search' />
+      <SideBarOptions Icon={LibraryMusicIcon} title='Your Library' />
+      <br />
+      <strong className='sideBar__title'>PLAYLISTS</strong>
+
+      <hr />
+      <SideBarOptions title='Hip Hop' />
+      <SideBarOptions title='Hip Hop' />
+      <SideBarOptions title='Pop' />
     </div>
   );
 }
--- src/components/SideBarOption.js
+++ src/components/SideBarOption.js
@@ -4,7 +4,8 @@ import '../css/SideBarOption.css';
 function SideBarOptions({title, Icon}) {
     return (
         <div className='sidebarOption'>
-            <p>{title}</p>
+            {Icon && <Icon className='sidebarOption__icon'/>}
+            {Icon? <h4>{title}</h4>:<p>{title}</p>}
         </div>
     )
 }
