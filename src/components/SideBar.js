e0187d0366d46fbf8bf7f8f187e50f73980dbf5f
Prathibha Ratnayake <prsthibha@gmail.com>
2020-09-16 14:42:45 +0530

    sideBar__logo added

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,9 +2,11 @@ import React from "react";
 import "../css/SideBar.css";
 
 function SideBar() {
-  return <div className='sideBar'>
-<h1>sidebar</h1>
-  </div>;
+  return (
+    <div className='sideBar'>
+        <img className='sideBar__logo' src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
+    </div>
+  );
 }
 
 export default SideBar;
